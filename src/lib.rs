434eb89a928d3d1ea6761f8dfbd33be60cc6ca05
LU Jialin <luxxxlucy@gmail.com>
2024-04-24 20:09:16 -0700

    init with sauron hello world tutorial

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use sauron::{node, wasm_bindgen, Application, Cmd, Node, Program};
+
+struct App;
+
+impl Application for App {
+    type MSG = ();
+
+    fn view(&self) -> Node<()> {
+        node! {
+            <p>
+                "hello"
+            </p>
+        }
+    }
+
+    fn update(&mut self, _msg: ()) -> Cmd<()> {
+        Cmd::none()
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    Program::mount_to_body(App);
+}
